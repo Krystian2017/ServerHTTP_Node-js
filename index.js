7a80fd09c3136d5613761ebb835d09f12d3f7822
Krystian Bieniek <krystekk2013@gmail.com>
2018-05-30 18:53:20 +0200

    Add new repository

---
+++ index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var http = require('http');
+
+var server = http.createServer();
+
+fs.readFile('./index.html', 'utf-8', function(err, data) {
+  server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/hello') {
+      response.write(data);
+      response.end();
+    } else {
+      response.statusCode = 404;
+      response.write('<h1>404: Zła ścieżka!</h1>');
+      response.end();
+    }
+  });
+server.listen(8080);
+});
